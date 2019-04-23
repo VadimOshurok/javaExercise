3161a8153d35c314ea23fdb323792697d2309027
VadimOshurok <49399798+VadimOshurok@users.noreply.github.com>
2019-04-23 16:09:01 +0300

    Update Main.java
    Задание 3

--- Exersice3/Main.java
+++ Exersice3/Main.java
@@ -6,6 +6,10 @@ public class Main {
        Rouse ros1 = new Rouse(10, "Роза", "Красная");
        Rouse ros2 = new Rouse(7.5, "Роза", "Белая");
        Violet vio1 = new Violet(5, "Фиалка", "Желтая");
+       Violet vio2 = new Violet(5, "Фиалка", "Розовая");
+       Tulip tul1 = new Tulip(1.5, "Тюльпан","Желтый");
+       Tulip tul2 = new Tulip(1.5, "Тюльпан","Желтый");
+       Tulip tul3 = new Tulip(1.5, "Тюльпан","Желтый");
        System.out.println(Flower.total+" руб.");
    }
 }
