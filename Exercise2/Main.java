c7b6b93f790293f735f42b57b819f9f6ef6bbe4c
Vadim <ERGHOSTIK@gmail.com>
2019-04-23 09:57:49 +0300

    Задание 2

---
+++ Exercise2/Main.java
@@ -0,0 +1,11 @@
+package Exercise2;
+
+public class Main {
+	public static void main(String[] args) {
+		IntRandomUtil.numbers();
+		IntRandomUtil.getComparison();
+		IntRandomUtil.getSumAllNum();
+		IntRandomUtil.getSubtraction();
+		IntRandomUtil.getSumOneNum();
+	}
+}
---
+++ Exercise2/SetNumber.java
@@ -0,0 +1,7 @@
+package Exercise2;
+
+public class SetNumber {
+	public final static int A=RNDGenerator.getRandom();
+	public final static int B=RNDGenerator.getRandom();
+	public final static int C=RNDGenerator.getRandom();
+}
